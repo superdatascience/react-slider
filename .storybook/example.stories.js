5f9d2477e2ecfc4abceb9b31a6f64545715c3c64
sebaslh12 <sebaslh12@gmail.com>
2020-08-26 15:11:18 -0500

    Project setup.

---
+++ .storybook/example.stories.js
@@ -0,0 +1,12 @@
+import React, { Fragment, useState } from 'react';
+import ReactSlider from '../src';
+
+export default { title: 'ExampleComponent' };
+
+export const Example = () => {
+	return (
+		<Fragment>
+			<ReactSlider/>
+		</Fragment>
+	);
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+const ReactSlider = () => (
+	<div>
+		<h1>React slider</h1>
+	</div>
+);
+
+export default ReactSlider;
