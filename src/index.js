6d7eee49970fded427dea7be3f1c178485b4d13d
sebaslh12 <sebaslh12@gmail.com>
2020-08-27 12:02:59 -0500

    Implementing auto slider.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, cloneElement, useEffect } from "react";
 import "./styles.css";
 
-const ReactSlider = () => (
-	<div>
-		<h1>React slider</h1>
-	</div>
-);
+const ReactSlider = ({ carouselItems, ...rest }) => {
+	const [active, setActive] = useState(0);
+	let scrollInterval = null;
+
+	useEffect(() => {
+		scrollInterval = setTimeout(() => {
+			setActive((active + 1) % carouselItems.length);
+		}, 2000);
+		return () => clearTimeout(scrollInterval);
+	});
+
+	return (
+		<div className="carousel">
+			{carouselItems.map((item, index) => {
+				const activeStyle = active === index ? " visible" : "";
+				return cloneElement(item, {
+					...rest,
+					className: `carousel-item${activeStyle}`,
+					key: index
+				});
+			})}
+		</div>
+	);
+};
 
 export default ReactSlider;
